73e6db4f5e4d1db778ffb1ab54a7616ff23f5ef0
ShinNakajima128 <ukauka224@i.softbank.jp>
2021-08-04 15:36:10 +0900

    GameManagerとLoadSceneManagerのPrefabを作成した。

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : SingletonMonoBehaviour<GameManager>
+{
+    [SerializeField] int m_stomachGauge = 100;
+    int m_score = default;
+    bool InGame = false;
+
+    public int GetScore { get => m_score; }
+
+    private void Awake()
+    {
+        if (this != Instance)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void Update()
+    {
+       if (InGame)
+        {
+            if (m_stomachGauge <= 0) InGame = false;
+        }
+    }
+
+    /// <summary>
+    /// スコアを加算する
+    /// </summary>
+    /// <param name="value"> 加算する量 </param>
+    public void AddScore(int value)
+    {
+        m_score += value;
+    }
+
+    /// <summary>
+    /// 空腹ゲージを回復させる
+    /// </summary>
+    /// <param name="value"> 回復量 </param>
+    public void Recovery(int value)
+    {
+        m_stomachGauge += value;
+
+        if (m_stomachGauge >= 100)
+        {
+            m_stomachGauge = 100;
+        }
+    }
+
+    /// <summary>
+    /// ダメージを受ける
+    /// </summary>
+    /// <param name="value"> ダメージ量 </param>
+    public void Damage(int value)
+    {
+        m_stomachGauge -= value;
+    }
+}
