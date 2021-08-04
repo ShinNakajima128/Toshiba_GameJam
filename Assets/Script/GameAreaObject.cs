dad21f8b547c15867b3b2884c72e9b50fc6470e2
sorude26 <sorude26@gmail.com>
2021-08-04 14:58:10 +0900

    ゲーム画面外に出たら消えるスクリプトを追加した

---
+++ Assets/Script/GameAreaObject.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// ゲーム画面外に出たら消える
+/// </summary>
+public class GameAreaObject : MonoBehaviour
+{
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "GameArea")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
