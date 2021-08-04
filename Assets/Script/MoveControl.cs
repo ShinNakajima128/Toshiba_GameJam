4904afb0c061e2b112aea1e7c6f232351ccf5b0e
sorude26 <sorude26@gmail.com>
2021-08-04 14:36:13 +0900

    移動関係の基底クラスを追加した

---
+++ Assets/Script/MoveControl.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// 移動関係の基底クラス
+/// </summary>
+public abstract class MoveControl : MonoBehaviour
+{
+    private void OnEnable()
+    {
+        EventManager.OnGameEnd += GameEnd;
+    }
+    private void OnDisable()
+    {
+        EventManager.OnGameEnd -= GameEnd;
+    }
+
+    public abstract void GameEnd();
+}
