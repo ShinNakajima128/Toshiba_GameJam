fec7d277613f1d9294a12cad452810ebdb193f08
sorude26 <sorude26@gmail.com>
2021-08-04 15:15:20 +0900

    指定方向へ直進のみ行うスクリプトを追加した

---
+++ Assets/Script/SelectDirMove.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 指定方向へ直進のみ行う
+/// </summary>
+public class SelectDirMove : MoveControl
+{
+    Rigidbody m_rB;
+    [SerializeField] float m_moveSpeed = 2f;
+    [SerializeField] Vector3 m_dir = Vector3.back;
+    bool m_moveF; 
+    void Start()
+    {
+        m_moveF = true;
+        m_rB = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (!m_moveF) return;
+        m_rB.velocity = m_dir * m_moveSpeed;
+    }
+    public override void GameEnd()
+    {
+        m_moveF = false;
+        m_rB.velocity = Vector3.zero;
+    }
+}
