f60e797f7cfb8767a1a9667632d020f1bd0f3a5d
RyuKento <83810925+RyuKento@users.noreply.github.com>
2021-08-05 16:40:25 +0900

    リザルトシーンとデバッグ用に使ったシーンを追加しました

---
+++ Assets/Script/ScoreManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+    [SerializeField] Text m_score;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_score.text = GameManager.Instance.GetScore.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
