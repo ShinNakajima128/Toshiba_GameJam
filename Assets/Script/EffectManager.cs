5e7f9f29c422ddc59fc378c2348fb0b6b7287553
sorude26 <sorude26@gmail.com>
2021-08-05 10:32:29 +0900

    EffectManagerを追加した。エフェクトのPrefabを追加するごとに追加設定を行う

---
+++ Assets/Script/EffectManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum EffectType
+{
+    Explosion,
+}
+/// <summary>
+/// エフェクトを再生する
+/// </summary>
+public class EffectManager : MonoBehaviour
+{
+    public static EffectManager Instance { get; private set; }
+    [SerializeField] GameObject[] m_effectPrefabs;
+
+    public void PlayEffect(EffectType type,Vector3 pos)
+    {
+        Instantiate(m_effectPrefabs[(int)type]).transform.position = pos;
+    }
+}
