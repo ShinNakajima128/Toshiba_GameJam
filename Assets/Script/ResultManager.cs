9712f8147a4ed0a905da817965d2f38766e48848
RyuKento <83810925+RyuKento@users.noreply.github.com>
2021-08-17 17:55:02 +0900

    リザルトのコルーチン追加

---
+++ Assets/Script/ResultManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResultManager : MonoBehaviour
+{
+    [SerializeField] Text m_calc = default;
+    [SerializeField] Text m_resultText = default;
+    [SerializeField] GameObject m_selectPanel = default;
+    [SerializeField] float m_timer = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_calc.enabled = false;
+        m_resultText.enabled = false;
+        m_selectPanel.SetActive(false);
+
+        StartCoroutine(OnResult());
+    }
+    
+    IEnumerator OnResult()
+    {
+        yield return new WaitForSeconds(m_timer);
+        m_calc.enabled = true;
+        yield return new WaitForSeconds(m_timer);
+        m_resultText.enabled = true;
+        yield return new WaitForSeconds(m_timer);
+        m_selectPanel.SetActive(true);
+    }
+}
