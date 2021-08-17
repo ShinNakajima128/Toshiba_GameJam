fa1eb78722f40561cb737e7a056c223f31836227
sorude26 <sorude26@gmail.com>
2021-08-17 12:39:25 +0900

    laserBeamを作成した

---
+++ Assets/Script/LaserControl.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserControl : MonoBehaviour
+{
+    Animator m_anime;
+    bool m_shot;
+    private void Start()
+    {
+        m_anime = GetComponent<Animator>();
+        m_anime.Play("StandbyLaser");
+    }
+    public void ShotLaser(float time)
+    {
+        if (m_shot)
+        {
+            return;
+        }
+        m_anime.Play("StartLaser");
+        StartCoroutine(LaserLife(time));
+    }
+    IEnumerator LaserLife(float time)
+    {
+        m_shot = true;
+        yield return new WaitForSeconds(time);
+        m_anime.Play("EndLaser");
+        m_shot = false;
+    }
+}
