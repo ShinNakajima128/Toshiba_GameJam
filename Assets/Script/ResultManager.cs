6f4961a037e6be1c5aad78098213aee92ad3f6a4
RyuKento <83810925+RyuKento@users.noreply.github.com>
2021-08-19 00:17:12 +0900

    リザルト画面でスコアの計算と結果表示を追加しました

--- Assets/Script/ResultManager.cs
+++ Assets/Script/ResultManager.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 public class ResultManager : MonoBehaviour
 {
     [SerializeField] Text m_calc = default;
+    [SerializeField] Text m_difficulty = default;
     [SerializeField] Text m_resultText = default;
     [SerializeField] GameObject m_selectPanel = default;
     [SerializeField] float m_timer = 1;
@@ -13,6 +14,7 @@ public class ResultManager : MonoBehaviour
     void Start()
     {
         m_calc.enabled = false;
+        m_difficulty.enabled = false;
         m_resultText.enabled = false;
         m_selectPanel.SetActive(false);
 
@@ -23,6 +25,8 @@ public class ResultManager : MonoBehaviour
     {
         yield return new WaitForSeconds(m_timer);
         m_calc.enabled = true;
+        yield return new WaitForSeconds(m_timer);
+        m_difficulty.enabled = true;
         yield return new WaitForSeconds(m_timer);
         m_resultText.enabled = true;
         yield return new WaitForSeconds(m_timer);
--- Assets/Script/ScoreManager.cs
+++ Assets/Script/ScoreManager.cs
@@ -6,11 +6,59 @@ using UnityEngine.UI;
 public class ScoreManager : MonoBehaviour
 {
     [SerializeField] Text m_score;
-    // Start is called before the first frame update
+    [SerializeField] Text m_difficulty;
+    [SerializeField] Text m_resultScore;
+    int m_scoreCopy;
+    string m_difficultyCopy;
+    float m_result;
+    public enum DifficultyRate
+    {
+        None,
+        Easy,
+        Normal,
+        Hard,
+        VeryHard,
+        Pirate
+    }
     void Start()
     {
         m_score.text = GameManager.Instance.GetScore.ToString();
+        m_difficultyCopy = ProgressManager.Instance.GetDifficulty.ToString();
+        m_difficulty.text = m_difficultyCopy;
+        m_scoreCopy = GameManager.Instance.GetScore;
+        m_result = Calculate(m_difficultyCopy);
+        m_resultScore.text = m_result.ToString();
     }
+
+    public float Calculate(string difficulty)
+    {
+        float result = default;
+        float rate = default;
+        switch (difficulty)
+        {
+            case "Easy":
+                rate = 1;
+                break;
+            case "Normal":
+                rate = 1.25f;
+                break;
+            case "Hard":
+                rate = 1.5f;
+                break;
+            case "VeryHard":
+                rate = 1.75f;
+                break;
+            case "Pirate":
+                rate = 2;
+                break;
+            default:
+                break;
+        }
+        result = m_scoreCopy * rate;
+
+        return result;
+    } 
+
 
     // Update is called once per frame
     void Update()
