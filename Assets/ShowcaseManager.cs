018c197dd2a20e51e51cbf7c4c6b1b162b9b9933
ShinNakajima128 <ukauka224@i.softbank.jp>
2021-08-20 10:51:39 +0900

    2D、3Dデザインの一覧を表示するModelShowcaseSceneを作成した

---
+++ Assets/ShowcaseManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowcaseManager : MonoBehaviour
+{
+    [SerializeField] GameObject m_panel = default;
+
+    public void ActiveSwitch()
+    {
+        if (m_panel.activeSelf)
+        {
+            m_panel.SetActive(false);
+        }
+        else if (!m_panel.activeSelf)
+        {
+            m_panel.SetActive(true);
+        }
+    }
+}
